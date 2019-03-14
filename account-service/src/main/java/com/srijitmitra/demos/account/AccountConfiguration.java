4395855cf35dbc3288f7ebf272fc1a4cbacd667a
Srijit Mitra <srijit@amazon.com>
2019-03-14 18:23:35 -0400

    Adding retries

--- account-service/src/main/java/com/srijitmitra/demos/account/AccountConfiguration.java
+++ account-service/src/main/java/com/srijitmitra/demos/account/AccountConfiguration.java
@@ -2,8 +2,10 @@ package com.srijitmitra.demos.account;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
+@EnableRetry
 @ComponentScan("com.srijitmitra.demos.account")
 public class AccountConfiguration {
 
--- customer-service/src/main/java/com/srijitmitra/demos/customer/CustomerConfiguration.java
+++ customer-service/src/main/java/com/srijitmitra/demos/customer/CustomerConfiguration.java
@@ -2,8 +2,10 @@ package com.srijitmitra.demos.customer;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
+@EnableRetry
 @ComponentScan("com.srijitmitra.demos.customer")
 public class CustomerConfiguration {
 	
